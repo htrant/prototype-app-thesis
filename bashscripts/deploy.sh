eed45bd672abe344d05e8e7292633559ac60e521
htrant <trunghieu.tran138@gmail.com>
2017-04-17 18:37:59 +0300

    tune up deploy scripts and pipelines

--- bashscripts/deploy.sh
+++ bashscripts/deploy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
 SERVICE_STAGE=$1
+SERVICE_REGION=eu-west-1
 
 if [ $# -eq 0 ]; then
-  echo "No stage is specified"
+  echo "No service stage is specified"
   exit 1
-fi
-
-if [ $SERVICE_STAGE = 'prod' ]; then
-  node_modules/.bin/serverless deploy --stage prod
-elif [ $SERVICE_STAGE = 'dev' ]; then
-  serverless deploy --stage dev
+elif [ $SERVICE_STAGE = 'prod' ] || [ $SERVICE_STAGE = 'staging' ]; then
+  node_modules/.bin/serverless deploy --stage $SERVICE_STAGE --region $SERVICE_REGION
 else
-  node_modules/.bin/serverless deploy --stage $SERVICE_STAGE
+  echo "Invalid service stage"
+  exit 1
 fi
