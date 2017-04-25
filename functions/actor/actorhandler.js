b01727a94812a8d60bc4f477d8879aeabdeaf60c
htrant <trunghieu.tran138@gmail.com>
2017-04-25 23:17:05 +0300

    [feature/actor-endpoint]: create api and function for getActorById

--- functions/actor/actorhandler.js
+++ functions/actor/actorhandler.js
@@ -1,13 +1,26 @@
 const Actor = require('../../models').Actor;
 
+const lambdaResponse = (object) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(object)
+  };
+};
+
 module.exports.getAllActors = (event, context, callback) => {
-  Actor.fidnAll()
+  Actor.findAll()
     .then((actors) => {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify(actors)
-      };
-      callback(null, response);
+      callback(null, lambdaResponse(actors));
+    })
+    .catch((exp) => {
+      callback(exp);
+    });
+};
+
+module.exports.getActorById = (event, context, callback) => {
+  Actor.findById(event.id)
+    .then((actor) => {
+      callback(null, lambdaResponse(actor));
     })
     .catch((exp) => {
       callback(exp);
