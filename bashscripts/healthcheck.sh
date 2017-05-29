48aeef6a09379608f2b240486df677153b4ce8f2
htrant <trunghieu.tran138@gmail.com>
2017-05-29 23:56:43 +0300

    [feature/PTTA-19]: fix typo

--- bashscripts/healthcheck.sh
+++ bashscripts/healthcheck.sh
@@ -5,7 +5,7 @@ echo "Doing health check..."
 
 HTTP_CODE=`curl -X GET -H "x-api-key:api_key_if_necessary" -m 2 -I $HEALTH_CHECK_URL 2>/dev/null | head -n 1 | awk -F" " '{print $2}'`
 
-if [ "$HTTP_CODE" == "200" ]; then
+if [ "$HTTP_CODE" = "200" ]; then
   echo "$(tput setaf 2)Passed health check"
   tput setaf 7
   exit 0
