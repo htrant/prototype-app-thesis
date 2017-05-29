90b1450bb8144790cdebed8b68444fec1c6e8eed
htrant <trunghieu.tran138@gmail.com>
2017-05-29 23:43:54 +0300

    [feature/PTTA-19]: add health check

--- bashscripts/pipeline.sh
+++ bashscripts/pipeline.sh
@@ -2,9 +2,11 @@
 
 ENV=$1
 DBCONN=$2
+PROBEURL=$3
 
 npm install --quiet
 npm run style
 
 sh ./bashscripts/setdb.sh $ENV $DBCONN
 sh ./bashscripts/deploy.sh $ENV
+sh ./bashscripts/healthcheck.sh $PROBEURL
